e320530d98ee852b1f626b8ee235f570a2744378
Mati Dastugue <matias.dastugue@altoros.com>
2019-09-02 15:28:33 -0300

    Add styled components

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import styled from 'styled-components'
+
+import GlobalStyle from '../../theme/globalStyle'
 
 import Home from '../Home'
+import Breadcrumb from '../../components/Breadcrumb'
+import Header from '../../components/Header'
 
-import './index.css'
+const items = [{ to: '/', label: 'Dashboard' }]
+
+const AppWrapper = styled.div`
+  background-color: #fafafa;
+`
 
 function App() {
   return (
-    <div className="App">
-      <p>Maker Governance Dashboard</p>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </div>
+    <Fragment>
+      <GlobalStyle />
+      <AppWrapper>
+        <Breadcrumb>
+          <Header items={items} />
+        </Breadcrumb>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </AppWrapper>
+    </Fragment>
   )
 }
 
