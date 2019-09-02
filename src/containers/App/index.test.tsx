140022f8e31211b41338bdcdd662771e2ede1fad
Lisandro Corbalan <lmcorbalan@gmail.com>
2019-09-02 15:15:08 -0300

    Notifications test

--- src/containers/App/index.test.tsx
+++ src/containers/App/index.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './index'
+import { BrowserRouter } from 'react-router-dom'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    div,
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
