08249d5795c492d110d652d885b8ef9f9838ce8c
Gabriel Rodriguez Alsina <gabriel.rodriguez@altoros.com>
2019-11-01 21:36:09 -0300

    (fix) time left graph

--- src/components/PollDetails/Charts/TimeLeftChart.tsx
+++ src/components/PollDetails/Charts/TimeLeftChart.tsx
@@ -22,7 +22,24 @@ const Title = styled.h4`
   text-transform: uppercase;
 `
 
-const ValueTitleWrapper = styled.div``
+const ValueTitleWrapper = styled.div`
+  margin: 0 22px 0 0;
+
+  &:last-child {
+    margin-right: 0;
+  }
+`
+
+const ValueRow = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  margin: 0 0 11px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`
 
 const OuterWrapper = styled.div`
   align-items: center;
@@ -36,6 +53,7 @@ const ValuesWrapper = styled.div`
   align-items: center;
   display: flex;
   height: 100%;
+  flex-direction: column;
   justify-content: center;
   width: 100%;
   z-index: 10;
@@ -53,10 +71,8 @@ const MainTitle = styled.h2`
 
 const TimeLeftChart = props => {
   const { data } = props
-
-  console.log(data[0])
-  console.log('xxxxx')
-  console.log(+data.value)
+  const isEnded: boolean = data[0].text === 'Ended'
+  const value = isEnded ? 100 : data[0].value
 
   return (
     <>
@@ -64,30 +80,36 @@ const TimeLeftChart = props => {
         <CardTitle content="Time Left" />
       </span>
       <OuterWrapper>
-        <ProgressCircle borderWidth={40} dimensions={250} progress={+data.value}>
+        <ProgressCircle borderWidth={40} dimensions={250} progress={value}>
           <ValuesWrapper>
-            {data[0].text === 'Ended' ? (
+            {isEnded ? (
               <MainTitle>{data[0].text}</MainTitle>
             ) : (
               <>
                 {data[0].time.days >= 0 && (
-                  <ValueTitleWrapper>
-                    <Value>{data[0].time.days}</Value>
-                    <Title>{data[0].time.days === 1 ? 'DAY' : 'DAYS'}</Title>
-                  </ValueTitleWrapper>
+                  <ValueRow>
+                    <ValueTitleWrapper>
+                      <Value>{data[0].time.days}</Value>
+                      <Title>{data[0].time.days === 1 ? 'DAY' : 'DAYS'}</Title>
+                    </ValueTitleWrapper>
+                  </ValueRow>
                 )}
-                {data[0].time.hours >= 0 && (
-                  <ValueTitleWrapper>
-                    <Value>{data[0].time.hours}</Value>
-                    <Title>{data[0].time.hours === 1 ? 'HOUR' : 'HOURS'}</Title>
-                  </ValueTitleWrapper>
-                )}
-                {data[0].time.minutes >= 0 && (
-                  <ValueTitleWrapper>
-                    <Value>{data[0].time.minutes}</Value>
-                    <Title>{data[0].time.minutes === 1 ? 'MINUTE' : 'MINUTES'}</Title>
-                  </ValueTitleWrapper>
-                )}
+                <ValueRow>
+                  <>
+                    {data[0].time.hours >= 0 && (
+                      <ValueTitleWrapper>
+                        <Value>{data[0].time.hours}</Value>
+                        <Title>{data[0].time.hours === 1 ? 'HOUR' : 'HOURS'}</Title>
+                      </ValueTitleWrapper>
+                    )}
+                    {data[0].time.minutes >= 0 && (
+                      <ValueTitleWrapper>
+                        <Value>{data[0].time.minutes}</Value>
+                        <Title>{data[0].time.minutes === 1 ? 'MINUTE' : 'MINUTES'}</Title>
+                      </ValueTitleWrapper>
+                    )}
+                  </>
+                </ValueRow>
               </>
             )}
           </ValuesWrapper>
