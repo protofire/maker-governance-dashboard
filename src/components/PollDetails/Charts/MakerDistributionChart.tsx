c279bdd0a6b838f81261176c6f1c20797de798ae
Mati Dastugue <matias.dastugue@altoros.com>
2019-11-07 12:55:06 -0300

    Set show/hide series charts + chart scaling + selected opacity

--- src/components/PollDetails/Charts/MakerDistributionChart.tsx
+++ src/components/PollDetails/Charts/MakerDistributionChart.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Line, YAxis } from 'recharts'
 import { defaultColors } from './'
 import { Chart, ChartWrapper } from '../../common'
 
 const MakerDistributionChart = props => {
+  const [selectedLines, setSelectedLine] = useState<any>([])
+  const [opacities, setOpacities] = useState({})
+  const getOpacities = opacities => setOpacities(opacities)
+  const selectLine = e => {
+    let selected = selectedLines.includes(e.value)
+      ? selectedLines.filter(line => line !== e.value)
+      : [...selectedLines, e.value.trim()]
+    setSelectedLine(selected)
+  }
   const { wrapperProps, modalProps, options, colors } = props
   const chartColors = [...defaultColors, ...colors]
   return (
     <ChartWrapper {...wrapperProps} hideFilters>
-      <Chart scale="point" {...modalProps}>
+      <Chart getOpacity={getOpacities} handleLegend={selectLine} scale="point" {...modalProps}>
         <YAxis type="number" domain={[0, 'dataMax + 1000']} />
         {options.map((option, i) => (
           <Line
+            strokeOpacity={opacities[`${option}`]}
+            dataKey={selectedLines.length === 0 || !selectedLines.includes(`${option}`) ? `${option}` : ''}
             key={option}
             isAnimationActive={modalProps.data ? false : true}
             dot={false}
@@ -19,7 +30,6 @@ const MakerDistributionChart = props => {
             stroke={chartColors[i]}
             strokeWidth={2}
             type="monotone"
-            dataKey={`${option}`}
           />
         ))}
       </Chart>
--- src/components/PollDetails/Charts/VotersDistributionChart.tsx
+++ src/components/PollDetails/Charts/VotersDistributionChart.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Line, YAxis } from 'recharts'
 import { defaultColors } from './'
 import { Chart, ChartWrapper } from '../../common'
 
 const VotersDistributionChart = props => {
+  const [selectedLines, setSelectedLine] = useState<any>([])
+  const [opacities, setOpacities] = useState({})
+  const getOpacities = opacities => setOpacities(opacities)
+  const selectLine = e => {
+    let selected = selectedLines.includes(e.value)
+      ? selectedLines.filter(line => line !== e.value)
+      : [...selectedLines, e.value.trim()]
+    setSelectedLine(selected)
+  }
   const { wrapperProps, modalProps, options, colors } = props
   const chartColors = [...defaultColors, ...colors]
 
   return (
     <ChartWrapper {...wrapperProps} hideFilters>
-      <Chart scale="point" {...modalProps}>
+      <Chart getOpacity={getOpacities} handleLegend={selectLine} scale="point" {...modalProps}>
         <YAxis />
         {options.map((option, i) => (
           <Line
+            strokeOpacity={opacities[`${option}`]}
+            dataKey={selectedLines.length === 0 || !selectedLines.includes(`${option}`) ? `${option}` : ''}
             key={option}
             isAnimationActive={modalProps.data ? false : true}
             dot={false}
@@ -20,7 +31,6 @@ const VotersDistributionChart = props => {
             stroke={chartColors[i]}
             strokeWidth={2}
             type="monotone"
-            dataKey={`${option}`}
           />
         ))}
       </Chart>
