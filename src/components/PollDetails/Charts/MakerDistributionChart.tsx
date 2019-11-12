b3c5b3c1ca7fd903edc41b506cb5d934bf50e85d
Mati Dastugue <matias.dastugue@altoros.com>
2019-11-12 17:16:13 -0300

    Updated selected series

--- src/components/PollDetails/Charts/MakerDistributionChart.tsx
+++ src/components/PollDetails/Charts/MakerDistributionChart.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import { Line, YAxis } from 'recharts'
 import { defaultColors } from './'
 import { Chart, ChartWrapper } from '../../common'
+import { CustomSvg } from '../../common/Icon'
+
+const LegendLi = styled.li`
+  display: inline-block;
+  margin-right: 10px;
+  cursor: pointer;
+  opacity: ${props => (props.disabledValue ? '0.6' : '1')};
+`
 
 const MakerDistributionChart = props => {
   const [selectedLines, setSelectedLine] = useState<any>([])
@@ -15,9 +24,30 @@ const MakerDistributionChart = props => {
   }
   const { wrapperProps, modalProps, options, colors } = props
   const chartColors = [...defaultColors, ...colors]
+
+  const renderLegend = props => {
+    const { payload, onMouseEnter, onMouseLeave, onClick } = props
+    return (
+      <ul className="recharts-default-legend" style={{ listStyleType: 'none' }}>
+        {payload.map((entry, index) => (
+          <LegendLi
+            onMouseEnter={() => onMouseEnter(entry)}
+            onMouseLeave={() => onMouseLeave(entry)}
+            onClick={() => onClick(entry)}
+            disabledValue={selectedLines.includes(entry.value)}
+            className={`recharts-legend-item legend-item-${index}`}
+            key={`item-${index}`}
+          >
+            <CustomSvg color={entry.color} />
+            <span>{entry.value}</span>
+          </LegendLi>
+        ))}
+      </ul>
+    )
+  }
   return (
     <ChartWrapper {...wrapperProps} hideFilters>
-      <Chart getOpacity={getOpacities} handleLegend={selectLine} scale="point" {...modalProps}>
+      <Chart legend={renderLegend} getOpacity={getOpacities} handleLegend={selectLine} scale="point" {...modalProps}>
         <YAxis type="number" domain={[0, 'dataMax']} />
         {options.map((option, i) => (
           <Line
