53719969f9a635496e3fcad389a46711127115d7
Mati Dastugue <matias.dastugue@altoros.com>
2019-11-19 16:00:29 -0300

    Merged with master and updated legend styles

--- src/components/VoteDetails/Charts/ExecutiveVsHatChart.tsx
+++ src/components/VoteDetails/Charts/ExecutiveVsHatChart.tsx
@@ -1,40 +1,22 @@
 import React from 'react'
 import { Bar, Cell, YAxis } from 'recharts'
-import { Chart, ChartWrapper } from '../../common'
-
-const CustomSvg = (props: any) => (
-  <>
-    {/*
-   // @ts-ignore */}
-    <svg
-      className="recharts-surface"
-      width="14"
-      height="14"
-      viewBox="0 0 32 32"
-      version="1.1"
-      style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: '4px' }}
-    >
-      {/*
-   // @ts-ignore */}
-      <path stroke="none" fill={props.color} d="M0,4h32v24h-32z" className="recharts-legend-icon"></path>
-    </svg>
-  </>
-)
+import { Chart, ChartWrapper, LegendLi } from '../../common'
+import { CustomSvg } from '../../common/Icon'
 
 const renderLegend = props => {
   const { data } = props
   const casted = !!data.find(el => el.casted)
   const isNext = !!data.find(el => el.isNext)
   return (
-    <ul style={{ listStyleType: 'none' }}>
-      <li>
+    <ul className="recharts-default-legend" style={{ listStyleType: 'none', paddingLeft: 0 }}>
+      <LegendLi>
         <CustomSvg color={casted ? '#27a02c' : '#ffc353'} />
         <span>Executive vote</span>
-      </li>
-      <li>
+      </LegendLi>
+      <LegendLi>
         <CustomSvg color={'#a04827'} />
         <span>{isNext ? 'Next Highest Executive' : 'Current Hat'}</span>
-      </li>
+      </LegendLi>
     </ul>
   )
 }
